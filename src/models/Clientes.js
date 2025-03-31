6a717da3155cae77e9b90b333eb5140db82e4928
DENIS D ANGELO DA SILVEIRA <denis.dasilveira@senacsp.edu.br>
2025-03-31 20:12:28 -0300

    edição do arquivo Clientes.js

---
+++ src/models/Clientes.js
@@ -0,0 +1,48 @@
+/**
+ * Modelo de dados dos dados dos clientes
+ * Criação da coleção
+ */
+
+// importação dos recursos do moongose
+const {model, Schema} = require('mongoose')
+
+//criação da estrutura da criação
+const clienteSchema = new Schema({
+    nome: {
+        type: String
+    },
+    nasc:{
+        type: String
+    },
+    email: {
+        type: String
+    },
+    cpf: {
+        type: String
+    },
+    cep: {
+        type: String
+    },
+    rua: {
+        type: String
+    },
+    Num: {
+        type: String
+    },
+    complemento: {
+        type: String
+    },
+    bairro: {
+        type: String
+    },
+    cidade: {
+        type: String
+    },
+    uf: {
+        type: String
+    }
+
+}, {versionKey: false})
+
+//exportar o modelo de dados para o main
+module.exports = model('Notas', clienteSchema)
